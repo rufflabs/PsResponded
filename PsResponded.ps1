c7244f509cf1f1496cf3258e4094a2f2cb340c41
rufflabs <jetaylor@gmail.com>
2022-12-12 06:17:44 -0600

    Removed process blocks and minor re-write of logic around Unregister switch.

--- PsResponded.ps1
+++ PsResponded.ps1
@@ -58,97 +58,97 @@ param(
     [switch]$Unregister
 )
 
-begin {
-    $EventSource = "PsResponded" # Report as this Source in Event Logs
-    $EventLog = "Application"    # The Event Log to write to.
-    $EventIdMessage = 5005       # Event ID for general messages.
-    $EventIdResponded = 5006     # Event ID for when a Responder-esque response is seen.
+$EventSource = "PsResponded" # Report as this Source in Event Logs
+$EventLog = "Application"    # The Event Log to write to.
+$EventIdMessage = 5005       # Event ID for general messages.
+$EventIdResponded = 5006     # Event ID for when a Responder-esque response is seen.
 
-    if(-not $Unregister) {
-        # Confirm $Hostname doesn't currently exist.
-        $Request = Resolve-DnsName -Name $Hostname -LlmnrNetbiosOnly -ErrorAction SilentlyContinue
-        
-        if($Request) {
-            $SourceIP = ($Request | Select-Object -ExpandProperty IPAddress) -join '|'
-            Write-Error -Message "Indicated hostname '$($Hostname)' is currently resolving to $($SourceIP).`nPsResponded is exiting."
-            exit
-        }
+function Test-Administrator {
+    <#
+    .SYNOPSIS
+    Returns true if the current user has Administrator privileges, false otherwise.
+    #>
+    $CurrentUser = [Security.Principal.WindowsIdentity]::GetCurrent()
+    (New-Object Security.Principal.WindowsPrincipal $CurrentUser).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
 
-        function Test-Administrator {
-            <#
-            .SYNOPSIS
-                Returns true if the current user has Administrator privileges, false otherwise.
-            #>
-            $CurrentUser = [Security.Principal.WindowsIdentity]::GetCurrent()
-            (New-Object Security.Principal.WindowsPrincipal $CurrentUser).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-        }
-    
-        if(Test-Administrator) {
-            $IsAdministrator = $true
-        }else{
-            $IsAdministrator = $false
-        }
-    
-        if($IsAdministrator) {
-            # Setup Event Source if needed
-            if([System.Diagnostics.EventLog]::SourceExists($EventSource)){
-                # If the Event Source is already registered, use the Event Log it's registered to.
-                $EventLog = [System.Diagnostics.EventLog]::LogNameFromSourceName($EventSource,"localhost")
-                Write-Output "Logging to $($EventLog) Event Log as Event Source $($EventSource)."
-            }else{
-                # Register the Event Source to the specified Event Log if it isn't already registered.
-                New-EventLog -LogName $EventLog -Source $EventSource
-                Write-Output "Registered Event Source ($($EventSource)) in Event Log ($($EventLog))"
-            }
-        }else{
-            Write-Warning -Message "Not running with Administrator privileges. Events will NOT be written to the Event Log!"
-        }
+if(Test-Administrator) {
+    $IsAdministrator = $true
+}else{
+    $IsAdministrator = $false
+}
 
-        $Message = "Starting PsResponded requesting LLMNR for hostname: $($Hostname)."
-        if($IsAdministrator) {
-            Write-EventLog -Message $Message -Source $EventSource -LogName $EventLog -EventId $EventIdMessage -EntryType Information
-        }else{
-            Write-Output -InputObject $Message
-        }
-        Write-Verbose -Message $Message
+if($Unregister) {
+    if($IsAdministrator) {
+        # Remove the Event Source registration
+        Remove-EventLog -Source $EventSource
+        Write-Output "Event Source $($EventSource) has been unregistered from Event Logs."
+        exit
+    }else{
+        Write-Error -Message "Please re-run as Administrator. Unable to remove Event Source without Administrator privileges."
+        exit
     }
 }
 
-process {
-    if($Unregister) {
-        if($IsAdministrator) {
-            # Remove the Event Source registration
-            Remove-EventLog -Source $EventSource
-            Write-Output "Event Source $($EventSource) has been unregistered from Event Logs."
-            exit
-        }else{
-            Write-Error -Message "Please re-run as Administrator. Unable to remove Event Source without Administrator privileges."
-            exit
-        }
+# Confirm $Hostname doesn't currently exist.
+$Request = Resolve-DnsName -Name $Hostname -LlmnrNetbiosOnly -ErrorAction SilentlyContinue
+
+if($Request) {
+    $SourceIP = ($Request | Select-Object -ExpandProperty IPAddress) -join '|'
+    Write-Error -Message "Indicated hostname '$($Hostname)' is currently resolving to $($SourceIP).`nPsResponded is exiting, please wait for cache to clear or use a different hostname."
+    exit
+}
+
+# Setup Event Source in Event Log if able
+if($IsAdministrator) {
+    if([System.Diagnostics.EventLog]::SourceExists($EventSource)){
+        # If the Event Source is already registered, use the Event Log it's registered to.
+        $EventLog = [System.Diagnostics.EventLog]::LogNameFromSourceName($EventSource,"localhost")
+        Write-Output "Logging to $($EventLog) Event Log as Event Source $($EventSource)."
+    }else{
+        # Register the Event Source to the specified Event Log
+        New-EventLog -LogName $EventLog -Source $EventSource
+        Write-Output "Registered Event Source ($($EventSource)) in Event Log ($($EventLog))"
     }
+}else{
+    Write-Warning -Message "Not running with Administrator privileges. Events will NOT be written to the Event Log!"
+}
 
-    while($true) {
-        $Request = Resolve-DnsName -Name $Hostname -LlmnrNetbiosOnly -ErrorAction SilentlyContinue
+# Log that PsResponded is starting
+$Message = "Starting PsResponded requesting LLMNR for hostname: $($Hostname)."
+if($IsAdministrator) {
+    Write-EventLog -Message $Message -Source $EventSource -LogName $EventLog -EventId $EventIdMessage -EntryType Information
+}else{
+    Write-Output -InputObject $Message
+}
+Write-Verbose -Message $Message
+
+while($true) {
+    # Perform LLMNR and NetBIOS lookup for specified hostname
+    $Request = Resolve-DnsName -Name $Hostname -LlmnrNetbiosOnly -ErrorAction SilentlyContinue
+    
+    if($Request) {
+        # Response was detected!
+        $SourceIP = ($Request | Select-Object -ExpandProperty IPAddress) -join '|'
+
+        $Message = "SourceIP=$($SourceIP);Message=Received response to mDNS request for $($Hostname) from $($SourceIP)."
         
-        if($Request) {
-            # Response was detected!
-            $SourceIP = ($Request | Select-Object -ExpandProperty IPAddress) -join '|'
-            $Message = "SourceIP=$($SourceIP);Message=Received response to mDNS request for $($Hostname) from $($SourceIP)."
-            if($IsAdministrator) {
-                Write-EventLog -Message $Message -Source $EventSource -LogName $EventLog -EventId $EventIdResponded -EntryType Information
-            }else{
-                Write-Output -InputObject "Detected response to mDNS request for $($Hostname) from $($SourceIP)."
-            }
-                
-            Write-Verbose -Message $Message
-
-            Start-Sleep -Seconds $DwellTime
+        # Log the response
+        if($IsAdministrator) {
+            Write-EventLog -Message $Message -Source $EventSource -LogName $EventLog -EventId $EventIdResponded -EntryType Information
         }else{
-            # Wait a random amount of time before the next request. This is to avoid anyone listening
-            # from determining if this is an automated request.
-            $WaitTime = Get-Random -Minimum $MinimumWait -Maximum $MaximumWait
-            Write-Verbose -Message "Waiting for $($WaitTime) seconds."
-            Start-Sleep -Seconds $WaitTime
+            Write-Output -InputObject "Detected response to mDNS request for $($Hostname) from $($SourceIP)."
         }
+        Write-Verbose -Message $Message
+
+        # Wait for dwell time until trying to again
+        Start-Sleep -Seconds $DwellTime
+    }else{
+        # Wait a random amount of time before the next request
+        $WaitTime = Get-Random -Minimum $MinimumWait -Maximum $MaximumWait
+
+        Write-Verbose -Message "Waiting for $($WaitTime) seconds."
+
+        Start-Sleep -Seconds $WaitTime
     }
 }
